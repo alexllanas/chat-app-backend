c4afb378b87147658837b086653c88517ec00bb0
alexllanas <81590832+alexllanas@users.noreply.github.com>
2025-07-23 21:07:22 -0500

    Change port

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import http from "http";
+
+const app = express();
+const serverPort = process.env.PORT || 80;
+export const server = http.createServer(app);
+
+app.get("/api/v1", (req, res) => {
+  res.send("Chat App");
+});
+
+app.post("/api/v1/register", (req, res) => {
+  // res.send(`${req}`);
+  console.log(`${req}`)
+});
+
+server.listen(serverPort, () => {
+  console.log(`HTTP and WebSocket server listening on port ${serverPort}`);
+});
