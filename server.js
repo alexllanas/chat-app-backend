7505a9498de4074a038c52e6fd347ad7002aeeb7
alexllanas <81590832+alexllanas@users.noreply.github.com>
2025-07-24 15:00:43 -0500

    Insert user into db.

--- server.js
+++ server.js
@@ -5,6 +5,42 @@ import bcrypt from "bcrypt";
 
 import {v4 as uuidv4} from "uuid";
 
+import { Client } from 'pg'
+import {config} from "dotenv";
+
+config();
+
+const client = new Client({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
+
+function insertUser(email, password) {
+  const insertQuery = `
+        INSERT INTO users (id, email, password, created_at)
+        VALUES (gen_random_uuid(), $1, $2, NOW())
+      `;
+
+  client.query(insertQuery, [email, password], (err, result) => {
+    if (err) {
+      console.error("Error executing query", err);
+    } else {
+      console.log("Query result:", result);
+    }
+  })
+}
+
+client.connect()
+    .then(() => {
+      console.log("Connected to DB");
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+
 const app = express();
 export const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
@@ -15,22 +51,23 @@ app.get("/api/v1", (req, res) => {
   res.send("Chat App");
 });
 
-function hashPassword(password) {
+async function hashPassword(password) {
   const saltRounds = 10;
-  return bcrypt.hash(password, saltRounds, (err, hash) => {
-      return hash
-  });
+  return await bcrypt.hash(password, saltRounds);
 }
 
-app.post("/api/v1/register", (req, res) => {
+app.post("/api/v1/register", async (req, res) => {
 
-  const hash = hashPassword(req.password);
-  const createdAt = new Date().toISOString();
+  const hash = await hashPassword(req.body.password);
+
+  insertUser(req.body.email, hash);
+
+  const result = await client.query("SELECT * FROM users")
 
   res.send({
-    "id": uuidv4(),
-    "hash": hash,
-    "createdAt": createdAt,
+    "id": result.rows[0].id,
+    "hash": result.rows[0].password,
+    "createdAt": result.rows[0].created_at,
   });
 });
 
