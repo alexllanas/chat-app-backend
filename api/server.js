68007bceb97ceaa9f90175713a49718c28614b78
alexllanas <81590832+alexllanas@users.noreply.github.com>
2025-07-24 11:36:08 -0500

    Combine servers

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import http from "http";
+import {WebSocketServer} from "ws";
 
 const app = express();
 export const server = http.createServer(app);
@@ -9,6 +10,42 @@ app.use(express.json());
 
 app.get("/api/v1", (req, res) => {
   res.send("Chat App");
+});
+
+// Websocket
+const wss = new WebSocketServer({ server });
+
+const clients = new Map(); // userId => WebSocket
+
+wss.on("connection", function connection(ws) {
+  console.log("Client connected");
+
+  ws.on("message", function incoming(message) {
+    try {
+      const data = JSON.parse(message);
+      console.log("received: ", data);
+      ws.send(`you sent data to the server.`);
+
+      if (data.type === "register" && data.userId) {
+        clients.set(data.userId, ws);
+        console.log(`Registered user ${data.userId}`);
+      }
+      if (data.type === 'message' && data.message && data.userId && data.toUserId) {
+        const client = clients.get(data.toUserId)
+        client.send(JSON.stringify(
+            {
+              fromUserId: data.userId,
+              message: data.message
+            }
+        ))
+      }
+
+    } catch (e) {
+      console.error("Invalid message", e);
+    }
+  });
+
+  ws.send("Connected to WebSocket server");
 });
 
 server.listen(PORT, '0.0.0.0', () => {
