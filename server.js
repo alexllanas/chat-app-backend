1733d983adc0acde7bc3113261bae0f333fa5317
alexllanas <81590832+alexllanas@users.noreply.github.com>
2025-07-26 13:17:33 -0500

    Add login endpoint

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ import express from "express";
 import http from "http";
 import {WebSocketServer} from "ws";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 import {v4 as uuidv4} from "uuid";
 
 import { Client } from 'pg'
 import {config} from "dotenv";
+import {authenticateToken} from "./middleware/auth.js";
 
 config();
 
@@ -18,19 +20,31 @@ const client = new Client({
   database: process.env.DB_NAME,
 });
 
-function insertUser(email, password) {
-  const insertQuery = `
-        INSERT INTO users (id, email, password, created_at)
-        VALUES (gen_random_uuid(), $1, $2, NOW())
+function insertUser(userId, email, hash, createdAt) {
+  const query = `
+        INSERT INTO users (id, email, password_hash, created_at)
+        VALUES ($1, $2, $3, $4)
       `;
 
-  client.query(insertQuery, [email, password], (err, result) => {
+  client.query(query, [userId, email, hash, createdAt], (err, result) => {
     if (err) {
       console.error("Error executing query", err);
     } else {
       console.log("Query result:", result);
     }
   })
+}
+
+async function hashPassword(password) {
+  const saltRounds = 10;
+  return await bcrypt.hash(password, saltRounds);
+}
+
+
+function getToken(userId, email) {
+  return jwt.sign({id: userId, email: email}, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRATION,
+  });
 }
 
 client.connect()
@@ -51,24 +65,64 @@ app.get("/api/v1", (req, res) => {
   res.send("Chat App");
 });
 
-async function hashPassword(password) {
-  const saltRounds = 10;
-  return await bcrypt.hash(password, saltRounds);
-}
-
 app.post("/api/v1/register", async (req, res) => {
+  const {email, password} = req.body;
+  if (!email || !password) { res.status(400).json({ error: "Missing email or password" }); }
 
-  const hash = await hashPassword(req.body.password);
+  const hash = await hashPassword(password);
+  const userId = uuidv4();
+  const createdAt = new Date().toISOString();
 
-  insertUser(req.body.email, hash);
+  try {
+    insertUser(userId, email, hash, createdAt);
 
-  const result = await client.query("SELECT * FROM users")
+    const token = jwt.sign({id: userId, email: email}, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRATION,
+    })
 
-  res.send({
-    "id": result.rows[0].id,
-    "hash": result.rows[0].password,
-    "createdAt": result.rows[0].created_at,
-  });
+    res.status(201).json({
+      "id": userId,
+      "email": email,
+      "access_token": token,
+      "created_at": createdAt,
+    });
+  } catch (err) {
+    console.error("Error registering user:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+
+});
+
+app.post("/api/v1/login",async (req, res) => {
+  const {email, password} = req.body;
+  if (!email || !password) {
+    return res.status(400).json({error: "Missing email or password"});
+  }
+
+  const query = `
+    SELECT id, password_hash
+    FROM users
+    WHERE email = $1
+  `
+  const result = await client.query(query, [email])
+  if (result.rowCount === 0) {
+    return res.status(401).json({error: "Invalid email or password"});
+  }
+  const user = result.rows[0];
+
+  const isMatch = await bcrypt.compare(password, user.password_hash);
+
+  if (!isMatch) {
+    return res.status(401).json({error: "Invalid email or password"});
+  }
+
+  const token = getToken(email);
+
+  res.json({
+    "id": user.id,
+    "email": email,
+    "access_token": token
+  })
 });
 
 // Websocket
