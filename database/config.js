ae03f944738f320186a6452e055eb951b6aa9352
alexllanas <81590832+alexllanas@users.noreply.github.com>
2025-07-28 17:20:53 -0500

    Configure remote db settings

--- database/config.js
+++ database/config.js
@@ -1,13 +1,13 @@
 import {Client} from "pg";
 import {config} from "dotenv";
 
-// config();
-//
-//
-// export const client = new Client({
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     database: process.env.DB_NAME,
-// });
+config();
+
+
+export const client = new Client({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+});
