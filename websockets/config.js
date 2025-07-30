4b4f7f0ed6bbc7cf0d33e0c11a8f213c8162b1f6
alexllanas <81590832+alexllanas@users.noreply.github.com>
2025-07-29 22:35:58 -0500

    Send additional message data

--- websockets/config.js
+++ websockets/config.js
@@ -7,29 +7,38 @@ export function setupWebSockets(server) {
     wss.on("connection", function connection(ws) {
         console.log("Client connected");
 
-        ws.on("message", function incoming(message) {
-            try {
-                const data = JSON.parse(message);
-                console.log("received: ", data);
-
-                if (data.type === "register" && data.user_id) {
-                    clients.set(data.user_id, ws);
-                    console.log(`Registered user ${data.user_id}`);
-                } else if (data.type === 'message' && data.content && data.sender_id && data.recipient_id) {
-                    const client = clients.get(data.recipient_id)
-                    client.send(JSON.stringify(
-                        {
-                            fromUserId: data.sender_id,
-                            message: data.content
-                        }
-                    ))
-                }
-
-            } catch (e) {
-                console.error("Invalid message", e);
-            }
-        });
+        ws.on("message", onMessage(clients, ws));
 
         ws.send("Connected to WebSocket server");
     });
+}
+
+
+function onMessage(clients, ws) {
+    return function incoming(message) {
+        try {
+            const data = JSON.parse(message);
+            console.log("received: ", data);
+
+            if (data.type === "register" && data.user_id) {
+                clients.set(data.user_id, ws);
+                console.log(`Registered user ${data.user_id}`);
+            } else if (data.type === 'message' && data.content && data.sender_id && data.recipient_id) {
+                const client = clients.get(data.recipient_id)
+                console.log("Client connected", client);
+                client.send(JSON.stringify(
+                    {
+                        id: data.id,
+                        senderId: data.sender_id,
+                        recipientId: data.recipient_id,
+                        content: data.content,
+                        timestamp: data.timestamp,
+                    }
+                ))
+            }
+
+        } catch (e) {
+            console.error("Invalid message", e);
+        }
+    };
 }
